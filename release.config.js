54ca913d47a4e807dd310e141178756245878c0b
hossein <hossien.alipour@gmail.com>
2020-09-22 00:00:07 +0300

    feat: config semantic

---
+++ release.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  dryRun: false,
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json", "yarn.lock"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
