185b49ae4c3e091e4af619ec897c937f2d3980e4
hossein <hossien.alipour@gmail.com>
2020-09-22 00:24:42 +0300

    fix: simplify release.config

--- release.config.js
+++ release.config.js
@@ -3,21 +3,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/changelog",
-      {
-        changelogFile: "CHANGELOG.md",
-      },
-    ],
-    "@semantic-release/npm",
-    "@semantic-release/github",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md", "package.json", "yarn.lock"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
+    "@semantic-release/changelog",
+    "@semantic-release/git",
   ],
 };
