a136a3b83f3e4ac665a2eddf982ba45e4832a41e
hossein <hossien.alipour@gmail.com>
2020-09-29 23:32:48 +0300

    Revert "remove release config"
    This reverts commit 8b441802a8a1c76b9bb0700b73faa3783579fb68.

---
+++ release.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  dryRun: false,
+  branches: ["master", "next"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "docs/CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["docs/CHANGELOG.md"],
+      },
+    ],
+  ],
+};
